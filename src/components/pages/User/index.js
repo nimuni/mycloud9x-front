42384d1febcb2726563d057315577020a5836de9
nimuni <kym9788@gmail.com>
2023-05-15 20:19:35 +0900

    feat: Theme 세팅 샘플

--- src/components/pages/User/index.js
+++ src/components/pages/User/index.js
@@ -3,10 +3,24 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
 
 import { changeUserPassword, changeUserNickname } from '@slice/userSlice';
 
-const theme = createTheme();
+// https://mui.com/material-ui/customization/theming/
+const theme = createTheme({
+  /* palette: { // 자주사용하는 컬러를 palette로 설정한다.
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+    typography: { // 폰트설정
+        fontFamily: ["Noto Sans KR", "sans-serif", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue"].join(",")
+    }
+  }, */
+});
 
 
 
